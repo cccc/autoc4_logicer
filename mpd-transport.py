26ee9c40536a11c92e31177327159e8e1678b25f
Ike <ike@koeln.ccc.de>
2015-04-21 19:31:38 +0200

    Add baellebad mpd

--- mpd-transport.py
+++ mpd-transport.py
@@ -21,6 +21,7 @@ CHANNEL_TO_SERVER = {
     # topic_part: (mpd_server_name, mpd_server_port, mpd_topic_prefix)
     'plenar': ('eeetop', 6600, 'mpd/plenar'),
     'fnord': ('trillian', 6600, 'mpd/fnord'),
+    'baellebad': ('autoc4', 6600, 'mpd/baellebad'),
 }
 
 ALLOWED_COMMANDS = {
